95c05e643c4fe81e2bc455978c27851cbde62268
Angel Ortega <cs.angelortega@gmail.com>
2017-10-23 17:47:00 -0700

    adding new config and README run steps

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spark-word2vec
 Come see these bad n'boujee flavors with apache spark and word2vec scala
+
+# Run from sbt console
+```
+cd build
+sbt
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 import org.apache.spark.SparkContext
+//import org.apache.spark.SparkConf
 
 object Main extends App {
-
+   val conf = new SparkContext
+  //val conf = SparkConf.setAppName("bad-n-boujee").setMaster("local[4]")
 }
